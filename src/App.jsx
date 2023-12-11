c4af612c646a518faa984b3f133d69c555a1a2d9
Rahul Patil <discoverrahul592@gmail.com>
2023-12-11 14:33:47 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,55 +9,55 @@ import Contact from './components/contact/Contact'
 import Background from './components/background/Background'
 import './App.css'
 import Footer from './components/footer/Footer'
-import { useState,useRef } from 'react'
+import { useState, useRef } from 'react'
 import Modal from './components/modal/Modal'
-import { ToastContainer, toast } from 'react-toastify';
+import toast, { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
 import emailjs from '@emailjs/browser';
-
 const App = () => {
   const [modal, setModal] = useState(false);
   const [modData, setModData] = useState(null);
   const form = useRef();
   const submitHandler = (event) => {
     event.preventDefault();
+
     emailjs.sendForm('service_yegchll', 'template_kvqosfp', form.current, '62snBK0RsvIeoiV6V')
       .then((result) => {
-      
-        toast.success('Message sent successfully !', {
-          position: toast.POSITION.TOP_CENTER
-      });
+
+        toast.success('Message sent successfully !')
+        event.target.reset();
       }, (error) => {
         console.log(error.text);
       });
+      
   }
 
   return (
     <>
       <Background />
       {
-        !modal &&   <Navbar />
+        !modal && <Navbar />
       }
-    
+
       {
-      modal && 
-        
+        modal &&
+
         <Modal data={modData} setModal={setModal} />
-        }
-        <ToastContainer
+      }
+      <div><Toaster
         position="top-center"
-        className='tst'
-       /> 
-        <div id='back'>
-          <Home />
-          <About />
-          <Skill />
-          <Project modal={modal} setModal={setModal} modData={modData} setModData={setModData}/>
-          <Testimonial />
-          <Contact form={form} submitHandler={submitHandler}/>
-          <Footer />
-        </div>
-    
+        reverseOrder={false}
+      /></div>
+      <div id='back'>
+        <Home />
+        <About />
+        <Skill />
+        <Project modal={modal} setModal={setModal} modData={modData} setModData={setModData} />
+        <Testimonial />
+        <Contact form={form} submitHandler={submitHandler} />
+        <Footer />
+      </div>
+
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,47 +1,48 @@
 import React from 'react'
 import { useState } from 'react'
 import './navbar.css'
-import {easeOut, motion} from 'framer-motion'
+import { easeOut, motion } from 'framer-motion'
 
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { GiHamburgerMenu } from 'react-icons/gi'
 const Navbar = () => {
-  const [showList,setShowList]=useState(false);
+  const [showList, setShowList] = useState(false);
 
   return (
-    <div id='nav'>  
+    <div id='nav'>
       <div id='app_logo'>Rahul</div>
-      <ul className={showList?'nav_list_mob':'nav_list'}>
-        <motion.li 
-        initial={{opacity:1,scale:1}}
-        whileHover={{opacity:1,scale:1.08}}
-        transition={{duration:0.2,ease:easeOut}} 
-        className="nav_li" ><a href="#home_sec">Home</a></motion.li>
-        <motion.li 
-        initial={{opacity:1,scale:1}}
-        whileHover={{opacity:1,scale:1.08}}
-        transition={{duration:0.2,ease:easeOut}} 
-        className='nav_li'><a href="#about_sec">About</a></motion.li>
-        <motion.li 
-        initial={{opacity:1,scale:1}}
-        whileHover={{opacity:1,scale:1.08}}
-        transition={{duration:0.2,ease:easeOut}} 
-        className='nav_li'><a href="#skill_sec">Skills</a></motion.li>
+      <ul className={showList ? 'nav_list_mob' : 'nav_list'}>
         <motion.li
-        initial={{opacity:1,scale:1}}
-        whileHover={{opacity:1,scale:1.08}}
-        transition={{duration:0.2,ease:easeOut}} 
-         className='nav_li'><a href="#proj_sec">Projects</a></motion.li>
-        <motion.li 
-        initial={{opacity:1,scale:1}}
-        whileHover={{opacity:1,scale:1.08}}
-        transition={{duration:0.2,ease:easeOut}} 
-        className='nav_li'><a href="#cnt_sec">Contact</a></motion.li>
+          initial={{ opacity: 1, scale: 1 }}
+          whileHover={{ opacity: 1, scale: 1.07 }}
+          transition={{ duration: 0.2, ease: easeOut }}
+          className="nav_li" ><a href="#home_sec">Home</a></motion.li>
+        <motion.li
+          initial={{ opacity: 1, scale: 1 }}
+          whileHover={{ opacity: 1, scale: 1.07 }}
+          transition={{ duration: 0.2, ease: easeOut }}
+          className='nav_li'><a href="#about_sec">About</a></motion.li>
+        <motion.li
+          initial={{ opacity: 1, scale: 1 }}
+          whileHover={{ opacity: 1, scale: 1.07 }}
+          transition={{ duration: 0.2, ease: easeOut }}
+          className='nav_li'><a href="#skill_sec">Skills</a></motion.li>
+        <motion.li
+          initial={{ opacity: 1, scale: 1 }}
+          whileHover={{ opacity: 1, scale: 1.07 }}
+          transition={{ duration: 0.2, ease: easeOut }}
+          className='nav_li'><a href="#proj_sec">Projects</a></motion.li>
+        <motion.li
+          initial={{ opacity: 1, scale: 1 }}
+          whileHover={{ opacity: 1, scale: 1.07 }}
+          transition={{ duration: 0.2, ease: easeOut }}
+          className='nav_li'><a href="#cnt_sec">Contact</a></motion.li>
       </ul>
       <div id='ham_menu'>
-        <a href="sd" onClick={(e)=>{
+        <a href="sd" onClick={(e) => {
           e.preventDefault();
-          setShowList(!showList)}}>
-        <GiHamburgerMenu id='ham_icon' size={'7vmin'}/>
+          setShowList(!showList)
+        }}>
+          <GiHamburgerMenu id='ham_icon' size={'7vmin'} />
         </a>
       </div>
     </div>
